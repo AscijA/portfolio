3e91cf787dfd7277e06104a0bffc8b3046b7a073
MaidAscic <ascija111@gmail.com>
2021-08-28 23:23:30 +0200

    Setup and Cleanup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Navbar from "./components/layout/navbar/Navbar";
+import "./assets/fonts.css";
+import Footer from "./components/layout/footer/Footer";
+import { Fragment } from "react";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Navbar />
+      <Footer />
+    </Fragment>
   );
 }
 
