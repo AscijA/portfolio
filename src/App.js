bb877e1342a4849fb649430929e7354c36ae2c50
MaidAscic <ascija111@gmail.com>
2021-08-29 23:15:33 +0200

    Minor bug fixes and resposnivnes on contact me page

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ function App() {
   return (
     <Fragment>
       <Navbar />
-      {/* <ContactMe/> */}
+      <ContactMe/>
       {/* <ErrorPage /> */}
-      {/* <Footer /> */}
+      <Footer />
     </Fragment>
   );
 }
