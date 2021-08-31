d26c4a0a0131405e5129a337020481a0850b2761
MaidAscic <ascija111@gmail.com>
2021-08-31 13:16:35 +0200

    Fixed bar width on different percentages

--- src/pages/AboutMe/Skills/SkillsList/SkillsList.js
+++ src/pages/AboutMe/Skills/SkillsList/SkillsList.js
@@ -1,14 +1,20 @@
 import classes from "./SkillsList.module.css";
 import SkillsListItem from "./SkillsListItem";
 const skills = [
-  "HTML/CSS",
-  "JavaScript", 
-  "React", 
-  "Node", 
+  { name: "HTML/CSS", amount: 90 },
+  { name: "JavaScript", amount: 80 },
+  { name: "React", amount: 75 },
+  { name: "Node", amount: 75 },
 ];
 const SkillsList = () => {
   const listItems = skills.map((skill) => {
-    return <SkillsListItem name={skill}  key={Math.random()}/>;
+    return (
+      <SkillsListItem
+        name={skill.name}
+        amount={skill.amount}
+        key={Math.random()}
+      />
+    );
   });
   return <div className={classes.list}>{listItems}</div>;
 };
