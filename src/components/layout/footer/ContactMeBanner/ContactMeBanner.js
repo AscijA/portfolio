2538f46e923f02de08fee7cf8f746a82daa73633
MaidAscic <ascija111@gmail.com>
2021-08-31 13:12:38 +0200

    Fixed responsivness for ContactMeBanner

--- src/components/layout/footer/ContactMeBanner/ContactMeBanner.js
+++ src/components/layout/footer/ContactMeBanner/ContactMeBanner.js
@@ -1,22 +1,38 @@
-import Button from "../../../UI/Button/Button";
+import { useEffect, useState } from "react";
+
 import classes from "./ContactMeBanner.module.css";
+import Button from "../../../UI/Button/Button";
 
 const ContactMeBanner = () => {
+  /* handles weird media query bug. On window resize, when hiding certain element, a weird
+  margine appears*/
+  const [shouldRender, setShouldRender] = useState(true);
+  useEffect(() => {
+    function handleResize() {
+      if (window.innerWidth < 1024) {
+        setShouldRender(false);
+      } else {
+        setShouldRender(true);
+      }
+    }
+    window.addEventListener("resize", handleResize);
+  });
+
+  const shittyshit = !shouldRender ? null : (
+    <div className={classes.ideaContainer}>
+      <p className={classes.ideaText}>You have an idea? I can help you.</p>
+    </div>
+  );
+
   return (
     <div className={classes.outerContainer}>
       <div className={classes.innerContainer}>
-        <div className={classes.equalContainer}>
+        <div className={classes.titleContainer}>
           <p className={classes.title}>Let's cooperate!</p>
         </div>
-        <div className={classes.equalContainer}>
-          <p className={classes.ideaText}>
-            You have an idea? I can help you.
-          </p>
-        </div>
-        <div className={classes.equalContainer}>
-          <div className={classes.buttonContainer}>
-            <Button label="Contact Me" buttonType="1"/>
-          </div>
+        {shittyshit}
+        <div className={classes.buttonContainer}>
+          <Button label="Contact Me" buttonType="1" />
         </div>
       </div>
     </div>
