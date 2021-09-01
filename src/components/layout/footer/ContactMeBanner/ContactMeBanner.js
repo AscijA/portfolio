19014f22d198e10f78bed396519a7a20f12df1a4
MaidAscic <ascija111@gmail.com>
2021-09-01 17:10:52 +0200

    Styling bugs fixed

--- src/components/layout/footer/ContactMeBanner/ContactMeBanner.js
+++ src/components/layout/footer/ContactMeBanner/ContactMeBanner.js
@@ -4,25 +4,6 @@ import classes from "./ContactMeBanner.module.css";
 import Button from "../../../UI/Button/Button";
 
 const ContactMeBanner = () => {
-  /* handles weird media query bug. On window resize, when hiding certain element, a weird
-  margine appears*/
-  const [shouldRender, setShouldRender] = useState(true);
-  const handleResize = () => {
-    if (window.innerWidth < 1024) {
-      setShouldRender(false);
-    } else {
-      setShouldRender(true);
-    }
-  };
-  useEffect(() => {
-    window.addEventListener("resize", handleResize);
-  }, [shouldRender]);
-
-  const shittyshit = !shouldRender ? null : (
-    <div className={classes.ideaContainer}>
-      <p className={classes.ideaText}>You have an idea? I can help you.</p>
-    </div>
-  );
 
   return (
     <div className={classes.outerContainer}>
@@ -30,7 +11,9 @@ const ContactMeBanner = () => {
         <div className={classes.titleContainer}>
           <p className={classes.title}>Let's cooperate!</p>
         </div>
-        {shittyshit}
+        <div className={classes.ideaContainer}>
+          <p className={classes.ideaText}>You have an idea? I can help you.</p>
+        </div>
         <div className={classes.buttonContainer}>
           <Button label="Contact Me" buttonType="1" />
         </div>
