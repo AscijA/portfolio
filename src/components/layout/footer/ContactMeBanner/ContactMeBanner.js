81005f32a4f881bac9e49a329d519044a34660f6
MaidAscic <ascija111@gmail.com>
2021-09-04 12:57:29 +0200

    Added Link for Contact me footer Added preventDefault() for ContactForm Send Button

--- src/components/layout/footer/ContactMeBanner/ContactMeBanner.js
+++ src/components/layout/footer/ContactMeBanner/ContactMeBanner.js
@@ -1,9 +1,8 @@
-
 import classes from "./ContactMeBanner.module.css";
 import Button from "../../../UI/Button/Button";
+import { Link } from "react-router-dom";
 
 const ContactMeBanner = () => {
-
   return (
     <div className={classes.outerContainer}>
       <div className={classes.innerContainer}>
@@ -13,9 +12,10 @@ const ContactMeBanner = () => {
         <div className={classes.ideaContainer}>
           <p className={classes.ideaText}>You have an idea? I can help you.</p>
         </div>
-        <div className={classes.buttonContainer}>
-          <Button label="Contact Me" buttonType="1" />
-        </div>
+
+          <Link to="/contact" className={classes.buttonContainer}>
+            <Button label="Contact Me" buttonType="1" />
+          </Link>
       </div>
     </div>
   );
--- src/pages/ContactMe/ContactForm.js
+++ src/pages/ContactMe/ContactForm.js
@@ -2,8 +2,11 @@ import classes from "./ContactForm.module.css";
 import Button from "../../components/UI/Button/Button"
 import Input from "./Input";
 const ContactForm = () => {
+  const onSendHandler = (event)=>{
+    event.preventDefault()
+  }
   return (
-    <form className={classes.formContainer}>
+    <form className={classes.formContainer} onSubmit={onSendHandler}>
         <h1 className={classes.title}>Contact Me</h1>
       <div className={classes.inputContainer}>
         <Input label="Name" type="text"/>
